5e9a8b7263d53fa8ab1929ed5f6a704976820e89
Erick Salignac <89262324+ericksalignac@users.noreply.github.com>
2025-01-03 16:48:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
+import json
 
 # Define the FastAPI application
 app = FastAPI(title="Lambda Response Receiver API", description="API to receive and process results from AWS Lambda.")
+
+def extrair_campos_e_formatar_reposta(response, retorno=False):
+    """
+    Recebe a resposta_do_modelo entre tags XML, extrai os campos e retorna o texto formatado adequadamente
+
+    Argumentos: 
+        resposta_do_modelo (str): texto a ser processado
+        retorno (bool): Se True irá retornar os campos extraídos em formato de dicionário
+
+    Retorna:
+        resposta_do_modelo_formatada (str): Resposta formatada do modelo
+        campos_da_resposta_do_modelo (dict) : se retorno == True retorna os campos extraídos da resposta
+
+    """
+
+    resposta_json = json.loads(response)
+    resposta_do_modelo = resposta_json["generated_response"]
+
+    # Parsear o XML
+    root = ET.fromstring(resposta_do_modelo)
+
+    # Função recursiva para extrair tags e conteúdos
+    def extract_tags(element):
+        campos_da_resposta_do_modelo = {}
+        for child in element:
+            tag_key = child.tag  # Apenas o nome da tag
+            campos_da_resposta_do_modelo[tag_key] = child.text.strip() if child.text else ""
+            # Adicionar conteúdo dos filhos, se houver
+            campos_da_resposta_do_modelo.update(extract_tags(child))
+        return campos_da_resposta_do_modelo
+
+    # Extrair todas as tags e conteúdos
+    campos_da_resposta_do_modelo = extract_tags(root)
+
+    # Gerar a resposta_do_modelo_formatada com título e subtítulos
+
+    resposta_do_modelo_formatada = "PARECER TÉCNICO\n\n"  # Título principal
+    for chave, conteudo in campos_da_resposta_do_modelo.items():
+        # Substituir "_" por espaço e capitalizar cada palavra para o subtítulo
+        subtitulo = chave.replace("_", " ").title()
+        resposta_do_modelo_formatada += f"{subtitulo}\n{'-' * len(subtitulo)}\n{conteudo}\n\n"
+
+    if retorno == True:
+        return resposta_do_modelo_formatada, campos_da_resposta_do_modelo 
+    else:
+        return resposta_do_modelo_formatada
+
+
+
 
 # Define the data model for incoming POST requests
 class LambdaResponse(BaseModel):
@@ -24,7 +74,7 @@ async def receive_lambda_response(response: LambdaResponse):
     """
     try:
         # Log or process the data here
-        print("Received response:", response)
+        print()
 
         return {
             "message": "Response received successfully.",
